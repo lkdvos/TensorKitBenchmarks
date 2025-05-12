b94538a727737c0aa83165c01d4925456f313a1d
Lukas Devos <ldevos98@gmail.com>
2025-05-12 12:53:04 -0400

    add benchmark script

---
+++ scripts/heisenberg_xxx_bench.jl
@@ -0,0 +1,105 @@
+using BenchmarkTools
+using TensorKit
+using TensorKit: dim
+using TOML
+
+# Utility functions
+# -----------------
+
+trivialspace(d::Int) = ComplexSpace(d)
+u1space(d::Dict) = u1space(d["sectors"], d["dims"])
+u1space(sectors, dims) = U1Space(U1Irrep(c / 2) => d for (c, d) in zip(sectors, dims))
+su2space(d::Dict) = su2space(d["sectors"], d["dims"])
+su2space(sectors, dims) = SU2Space(SU2Irrep((c - 1) / 2) => d for (c, d) in zip(sectors, dims))
+
+function instantiate_su2_spaces(d::Dict)
+    issetequal(keys(d), ("virtual_mps", "virtual_mpo", "physical")) || error("invalid dict")
+    Vmps = su2space.(d["virtual_mps"])
+    Vmpo = su2space.(d["virtual_mpo"])
+    Vphys = su2space.(d["physical"])
+    return Vphys, Vmps, Vmpo
+end
+function instantiate_u1_spaces(d::Dict)
+    issetequal(keys(d), ("virtual_mps", "virtual_mpo", "physical")) || error("invalid dict")
+    Vmps = u1space.(d["virtual_mps"])
+    Vmpo = u1space.(d["virtual_mpo"])
+    Vphys = u1space.(d["physical"])
+    return Vphys, Vmps, Vmpo
+end
+function instantiate_trivial_spaces(d::Dict)
+    issetequal(keys(d), ("virtual_mps", "virtual_mpo", "physical")) || error("invalid dict")
+    Vmps = trivialspace.(d["virtual_mps"])
+    Vmpo = trivialspace.(d["virtual_mpo"])
+    Vphys = trivialspace.(d["physical"])
+    return Vphys, Vmps, Vmpo
+end
+
+function init_tensors(T, Vphys, Vmps, Vmpo)
+    A = randn(T, Vmps[1] ⊗ Vphys[1] ⊗ Vmps[2]')
+    M = randn(T, Vmpo[1] ⊗ Vphys[1] ⊗ Vphys[1]' ⊗ Vmpo[2]')
+    FL = randn(T, Vmps[1] ⊗ Vmpo[1]' ⊗ Vmps[1]')
+    FR = randn(T, Vmps[2] ⊗ Vmpo[2] ⊗ Vmps[2]')
+    return A, M, FL, FR
+end
+
+function run_benchmark(A, M, FL, FR)
+    return @tensor FL[4, 2, 1] * A[1, 3, 6] * M[2, 5, 3, 7] * conj(A[4, 5, 8]) * FR[6, 7, 8]
+end
+
+function generate_benchmark(T, Vphys, Vmps, Vmpo)
+    local init
+    let T = T, Vphys = Vphys, Vmps = Vmps, Vmpo = Vmpo
+        init() = init_tensors(T, Vphys, Vmps, Vmpo)
+    end
+    @benchmarkable run_benchmark(A, M, FL, FR) setup = ((A, M, FL, FR) = $init())
+end
+
+function generate_benchmark!(suite, kind, T, params)
+    Vphys, Vmps, Vmpo = if kind == "su2"
+        instantiate_su2_spaces(params)
+    elseif kind == "u1"
+        instantiate_u1_spaces(params)
+    elseif kind == "trivial"
+        instantiate_trivial_spaces(params)
+    else
+        error()
+    end
+    Dphys = dim.(Vphys)
+    Dmps = dim.(Vmps)
+    Dmpo = dim.(Vmpo)
+    if max(Dmps...) > 20
+        return nothing
+    end
+    suite[(Dphys, Dmps, Dmpo)] = generate_benchmark(T, Vphys, Vmps, Vmpo)
+    return suite
+end
+
+# Parameters
+# ----------
+filename_in = joinpath(@__DIR__, "heisenberg_spaces.toml")
+filename_out = joinpath(@__DIR__, "heisenberg_results.json")
+eltypes = (Float64, ComplexF64)
+
+# Script
+# ------
+
+function (@main)(ARGS=[])
+    SUITE = BenchmarkGroup()
+
+    allparams = open(filename_in, "r") do io
+        return TOML.parse(io)
+    end
+
+    for (kind, params) in allparams
+        g = addgroup!(SUITE, kind)
+        for paramset in params
+            for T in eltypes
+                generate_benchmark!(g, kind, T, paramset)
+            end
+        end
+    end
+
+    results = run(SUITE; verbose=true)
+
+    BenchmarkTools.save(filename_out, results)
+end
